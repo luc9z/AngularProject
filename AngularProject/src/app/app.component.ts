b7d07b5a3ae4396959ea9890e1ec0a4c66e53075
Fred Brum <117548063+fredb05@users.noreply.github.com>
2025-05-04 14:40:05 -0300

    Bloco de Informações finalizado

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InfoComponent } from './info/info.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InfoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularProject';
