a9106011d65bab94bca8486680bb703bee1bcb7d
Lucas <131416070+luc9z@users.noreply.github.com>
2025-05-06 14:00:52 -0300

    mudanças no layout

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { InfoComponent } from './info/info.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { FormComponent } from './form/form.component';
+import { CarrosselComponent } from './carrossel/carrossel.component';
+
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [FormComponent, InfoComponent, CabecalhoComponent],
+  imports: [FormComponent, CarrosselComponent, CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
