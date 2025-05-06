ce300e17aeaa7838ce2f6803cdbbb8273369b576
Lucas <103782@urisantiago.br>
2025-05-06 16:16:04 -0300

    criação do footer e alteração do carrossel

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { InfoComponent } from './info/info.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { FormComponent } from './form/form.component';
 import { CarrosselComponent } from './carrossel/carrossel.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [FormComponent, CarrosselComponent, CabecalhoComponent],
+  imports: [FormComponent, CarrosselComponent, CabecalhoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
