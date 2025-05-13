8ada7426c195857b0121ed5d6df973ed9484e49d
Lucas <103782@urisantiago.br>
2025-05-13 17:44:10 -0300

    fix: arrumando a importação dos projetos

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, CabecalhoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
