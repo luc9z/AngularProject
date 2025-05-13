d965b3bb38643128df313224b3acc86b8b81d7a6
Erick Guedes Rosa <rosaerick53@gmail.com>
2025-05-13 10:30:08 -0300

    feat: added router pokemon

---
+++ AngularProject/src/app/pokemons/pokemons.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon',
+  standalone: true,
+  templateUrl: './pokemons.component.html',
+  styleUrls: ['./pokemons.component.css']
+})
+export class PokemonsComponent { }
