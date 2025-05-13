d5826128a5895a7141ea3b5ae58abd5e84b69bcf
Lucas <103782@urisantiago.br>
2025-05-13 16:23:08 -0300

    feat: pagina de Pokémons finalizada

--- AngularProject/src/app/pokemon/pokemon.component.ts
+++ AngularProject/src/app/pokemon/pokemon.component.ts
@@ -1,17 +1,110 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
+import { forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-selector: 'app-pokemon',
-templateUrl: './pokemon.component.html',
-styleUrls: ['./pokemon.component.css'],
+  selector: 'app-pokemon',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
-constructor(private pokemonService: PokemonService) {}
+  pokemons: any[] = [];
+  filteredPokemons: any[] = [];
+  allPokemonList: any[] = [];
+  isLoading = true;
+  searchTerm = '';
+  currentPage = 1;
+  totalPokemons = 0;
+  totalPages = 0;
 
-ngOnInit(): void {
-  this.pokemonService.getPokemonDetails(1).subscribe((data) => {
-    console.log(data);
-  });
-}
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+      this.pokemonService.getPokemonList(1, 0).subscribe(response => {
+        this.totalPokemons = response.count;
+        this.totalPages = Math.ceil(this.totalPokemons / 12);
+        this.loadPokemons();
+      });
+      this.loadAllPokemonNames();
+
+  }
+
+  loadPokemons() {
+    const offset = (this.currentPage - 1) * 12;
+    this.isLoading = true;
+
+    this.pokemonService.getPokemonList(12, offset).subscribe(response => {
+      const detailsRequests = response.results.map((pokemon: any) =>
+        this.pokemonService.getPokemonDetailsByUrl(pokemon.url)
+      );
+
+      forkJoin<any[]>(detailsRequests).subscribe((pokemonDetails) => {
+        this.pokemons = pokemonDetails.map(p => ({
+          name: p.name,
+          image: p.sprites.front_default,
+          stats: p.stats,
+        }));
+        this.filteredPokemons = [...this.pokemons];
+        this.isLoading = false;
+      });
+    });
+  }
+
+  loadAllPokemonNames() {
+    this.pokemonService.getAllPokemonNames().subscribe(response => {
+      this.allPokemonList = response.results;
+    });
+  }
+
+  search() {
+    const term = this.searchTerm.toLowerCase().trim();
+
+    if (!term) {
+      this.filteredPokemons = [...this.pokemons];
+      return;
+    }
+
+    this.isLoading = true;
+
+    const matchedPokemons = this.allPokemonList.filter((p: any) =>
+      p.name.toLowerCase().includes(term)
+    ).slice(0, 10);
+
+    if (matchedPokemons.length === 0) {
+      this.filteredPokemons = [];
+      this.isLoading = false;
+      return;
+    }
+
+    const detailsRequests = matchedPokemons.map(p =>
+      this.pokemonService.getPokemonDetailsByUrl(p.url)
+    );
+
+    forkJoin(detailsRequests).subscribe((pokemonDetails) => {
+      this.filteredPokemons = pokemonDetails.map(p => ({
+        name: p.name,
+        image: p.sprites.front_default,
+        stats: p.stats,
+      }));
+      this.isLoading = false;
+    });
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.loadPokemons();
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadPokemons();
+    }
+  }
 }
--- AngularProject/src/app/services/pokemon.service.ts
+++ AngularProject/src/app/services/pokemon.service.ts
@@ -3,15 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-providedIn: 'root',
+  providedIn: 'root',
 })
 export class PokemonService {
-private apiUrl = 'https://pokeapi.co/api/v2';
+  private apiUrl = 'https://pokeapi.co/api/v2';
 
-constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-getPokemonDetails(id: number): Observable<any> {
-  const url = `${this.apiUrl}/pokemon/${id}`;
-  return this.http.get(url);
-}
+  getPokemonList(limit = 50, offset = 0): Observable<any> {
+    return this.http.get(`${this.apiUrl}/pokemon?limit=${limit}&offset=${offset}`);
+  }
+
+  getPokemonDetailsByUrl(url: string): Observable<any> {
+    return this.http.get(url);
+  }
+
+  getAllPokemonNames(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/pokemon?limit=100000`);
+  }
 }
